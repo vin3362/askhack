8a1f73466c82ab71ed957a1e4176dfb666d673f9
Michael Mayfield <michael.mayfield@ask.com>
2011-12-15 15:23:32 -0800

    Added example call for tag object (localhost:3000/tag/123)

--- web/routes/tag.js
+++ web/routes/tag.js
@@ -1,14 +1,46 @@
+var http = require("http")
 
-/*
- * GET home page.
- */
+function getTag(tagId, callback) {
+    var connection = http.createClient(8000, 'localhost'),
+        request = connection.request('/tag/' + tagId);
+    
+    connection.addListener('error', function(connectionException){
+        console.log("Error: " + connectionException);
+    });
+
+    request.addListener('response', function(response){
+        var data = '';
+        
+        response.addListener('data', function(chunk){
+            data += chunk;
+        });
+        
+        response.addListener('end', function(){
+            // Do something with data.
+            callback(JSON.parse(data));
+        });
+    });
+
+    request.end();
+}
 
 exports.tag = function(req, res){
-  res.render('tag', {
-    locals: {
-        title: "Snap'n'Ask",
-        name: 'Mike',
-        tag_id: req.params.id
-    }
-});
+    getTag(req.params.id, function(tag){
+        if(tag) {
+            res.render('tag', {
+                locals: {
+                    title: "Snap'n'Ask: " + tag.name,
+                    name: tag.name,
+                    date: new Date(tag.date),
+                    username: tag.user.name
+                }
+            }); 
+        } else {
+            res.render('tag', {
+                    locals: {
+                        title: "Couldn't find tag for " + req.params.id
+                    }
+                });
+        }
+    });
 };
